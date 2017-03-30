34639afc5cdd98a035a65a28450134db0fa99054
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2017-03-30 19:02:20 +0530

    added basic directions panel

--- static_files/js/mapController.js
+++ static_files/js/mapController.js
@@ -1,11 +1,20 @@
-app.controller("mapController",function($scope,NgMap){
+app.controller("mapController",function($scope,NgMap,$window){
   var map;
+  //$scope.showDirections = false;
+  $scope.source = "";
+  $scope.destination="";
+
   NgMap.getMap().then(function(mapDetails){
     map = mapDetails;
   });
 
-  $scope.placeChanged = function(){
+  //function just for reference
+  $scope.PlaceChanged = function(){
     $scope.place = this.getPlace();
     console.log("Location: "+$scope.place.geometry.location);
+  }
+
+  $scope.logOff = function(){
+    $window.location.href="/";
   }
 })
